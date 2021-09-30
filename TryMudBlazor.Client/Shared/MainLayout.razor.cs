7095eaf8389db122810a5f322c938935ef0922f0
Jonny Larsson <jonny@gardnet.nu>
2021-09-30 08:25:29 +0200

    cleaned some stuff

--- TryMudBlazor.Client/Shared/MainLayout.razor.cs
+++ TryMudBlazor.Client/Shared/MainLayout.razor.cs
@@ -3,18 +3,13 @@
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
-    using TryMudBlazor.Client.Components;
     using Try.Core;
     using Microsoft.AspNetCore.Components;
 
-    public partial class MainLayout : IDisposable
+    public partial class MainLayout
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
-
-        private PageNotifications PageNotificationsComponent { get; set; }
-
-        public void Dispose() => this.PageNotificationsComponent?.Dispose();
 
         protected override async Task OnInitializedAsync()
         {
