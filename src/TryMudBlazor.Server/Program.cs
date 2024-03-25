a1c9dbaa56518c5ca0fbe82aad3ce9040d20dbeb
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-03-25 13:18:36 -0500

    revert

--- src/TryMudBlazor.Server/Program.cs
+++ src/TryMudBlazor.Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MudBlazor.Examples.Data;
 
 var builder = WebApplication.CreateBuilder(args);
